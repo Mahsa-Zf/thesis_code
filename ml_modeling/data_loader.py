6bd12ae7269aec59c76510dcaa82dfbd3b658885
MahsaZ <m.zamani.fard@umcg.nl>
2026-04-25 23:28:44 +0200

    fixed the indexing mismatch between targets and features, stemming from the data_loader script

--- ml_modeling/data_loader.py
+++ ml_modeling/data_loader.py
@@ -6,7 +6,7 @@ import yaml
 import pandas as pd
 from pathlib import Path
 
-# Define the path to the config.yaml file
+# Defining the path to the config.yaml file
 base_dir = Path(__file__).resolve().parent
 config_path = base_dir.parent / "config.yaml"
 def load_data(config_path=config_path):
@@ -34,20 +34,20 @@ def load_data(config_path=config_path):
     clinical = pd.read_pickle(config['clinical']['cleaned'])
     clinical.set_index('record_id', inplace=True)
 
+    # Keeping intersection of indices; order clinical by radiomics_delta order
+    common_idx = radiomics_delta.index.intersection(clinical.index)
+    clinical = clinical.reindex(common_idx)
+    # Creating a dataframe for the target variables and dropping them from the clinical dataframe to avoid data leakagg
     targets_to_drop = ["surv_bestresponse_car",
                        "ae_summ_icans_v2",
                        "ae_summ_icans_highestgrade_v2",
                        "ae_summ_crs_v2",
                        "ae_summ_highestgrade_v2"]
     targets = clinical[targets_to_drop]
-
-    for df in [radiomics_A, radiomics_B, radiomics_delta, clinical]:
-        df.drop(columns=targets_to_drop, inplace=True, errors='ignore')
-
-
-        X_clinical_A = clinical.join(radiomics_A, how='inner')
-        X_clinical_B = clinical.join(radiomics_B, how='inner')
-        X_clinical_Delta = clinical.join(radiomics_delta, how='inner')
+    clinical = clinical.drop(columns=targets_to_drop)
+    X_clinical_A = clinical.join(radiomics_A, how='inner')
+    X_clinical_B = clinical.join(radiomics_B, how='inner')
+    X_clinical_Delta = clinical.join(radiomics_delta, how='inner')
         
     return clinical, X_clinical_A, X_clinical_B, X_clinical_Delta, targets
 
